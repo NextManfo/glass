e4f0aa94f357b4bdcd76b1582beaa2c5716ca9de
Paolo Manfe <panfe.maolo@gmail.com>
2026-03-27 18:43:53 +0100

    fixed grandezza icone

--- main.go
+++ main.go
@@ -16,16 +16,27 @@ func main() {
 	time.Sleep(10 * time.Second) // Delay di 2 secondi
 	statusBar := display.StatusBar{Wifi: true, Battery: 100, Position: 10}
 	var currentScreen display.Screen = &display.HomeScreen{Title: "Home screen"}
+	tickDisplay := time.NewTicker(30 * time.Millisecond) // scroll fluido
+	tickStatus := time.NewTicker(1 * time.Second)        // statusbar e MQTT
+
 	for {
-		statusBar.Time = time.Now().Format("15:04")
-		dis.BeginDraw()
-		statusBar.Render(dis)
-		if scrollable, ok := currentScreen.(display.ScrollableScreen); ok {
-			scrollable.RenderScrollHorizontal(dis)
-		} else {
-			currentScreen.Render(dis)
+		select {
+		// case newMsg := <-msgChan:
+		//	currentScreen = &display.ChatScreen{Message: newMsg}
+
+		case <-tickStatus.C:
+			statusBar.Time = time.Now().Format("15:04")
+			// qui aggiorni wifi, batteria ecc
+
+		case <-tickDisplay.C:
+			dis.BeginDraw()
+			statusBar.Render(dis)
+			if scrollable, ok := currentScreen.(display.ScrollableScreen); ok {
+				scrollable.RenderScrollHorizontal(dis)
+			} else {
+				currentScreen.Render(dis)
+			}
+			dis.EndDraw()
 		}
-		dis.EndDraw()
-		time.Sleep(1 * time.Second)
 	}
 }
