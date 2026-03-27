00b95cb12f2971903ce8c37de008de5bd6f5349e
Paolo Manfe <panfe.maolo@gmail.com>
2026-03-27 16:59:53 +0100

    Aggiunta struttura cartelle e sviluppo premitive disegno display

---
+++ hw/display.go
@@ -0,0 +1,92 @@
+package hw
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"log"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	"periph.io/x/devices/v3/ssd1306"
+	"periph.io/x/devices/v3/ssd1306/image1bit"
+	"periph.io/x/host/v3"
+)
+
+type Display struct {
+	Device        *ssd1306.Dev
+	CurrentScreen *image1bit.VerticalLSB
+}
+
+func NewDisplay() *Display {
+	newDisplay := Display{}
+	// Init periph
+	_, err := host.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Open I2C
+	bus, err := i2creg.Open("1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bus.Close()
+
+	// Init display 128x32
+	dev, err := ssd1306.NewI2C(bus, &ssd1306.Opts{
+		W:       128,
+		H:       32,
+		Rotated: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inizializzato nuovo display: %v ", dev.Bounds().Size())
+	newDisplay.Device = dev
+	return &newDisplay
+}
+
+// Pulisce il dispay impostandolo tutto nero
+func (display *Display) ClearDisplay() {
+	draw.Draw(display.CurrentScreen, display.CurrentScreen.Bounds(), &image.Uniform{color.Black}, image.Point{}, draw.Src)
+}
+
+// Disegna le icone
+func (display *Display) DrawBitmap(bmp [][]int, offsetX, offsetY int) {
+	for y, row := range bmp {
+		for x, px := range row {
+			if px == 1 {
+				display.CurrentScreen.SetBit(offsetX+x, offsetY+y, true)
+			}
+		}
+	}
+}
+
+// Scrive del testo
+func (display *Display) AddLabel(x, y int, label string) {
+	col := color.Gray{Y: 255}
+
+	point := fixed.Point26_6{
+		X: fixed.I(x),
+		Y: fixed.I(y),
+	}
+
+	d := &font.Drawer{
+		Dst:  display.CurrentScreen,
+		Src:  image.NewUniform(col),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+
+	d.DrawString(label)
+}
+
+func (display *Display) BeginDraw() {
+	display.CurrentScreen = image1bit.NewVerticalLSB(display.Device.Bounds())
+	display.ClearDisplay() // Pulisco gia il display se rinizio a disegnare
+}
+
--- main.go
+++ main.go
@@ -1,87 +1,20 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"log"
 	"time"
 
-	"golang.org/x/image/font"
-	"golang.org/x/image/font/basicfont"
-	"golang.org/x/image/math/fixed"
-
-	"periph.io/x/conn/v3/i2c/i2creg"
-	"periph.io/x/devices/v3/ssd1306"
-	"periph.io/x/host/v3"
+	"glass/display"
+	"glass/hw"
 )
 
 func main() {
 	// Init periph
-	_, err := host.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
+	dis := hw.NewDisplay()
+	dis.BeginDraw()
+	dis.AddLabel(10, 30, "Inizializzazione display")
 
-	// Open I2C
-	bus, err := i2creg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer bus.Close()
-
-	// Init display 128x32
-	dev, err := ssd1306.NewI2C(bus, &ssd1306.Opts{
-		W:       128,
-		H:       32,
-		Rotated: true,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Crea buffer immagine
-	img := image.NewGray(image.Rect(0, 0, 128, 32))
-
-	// Colori
-	white := color.Gray{Y: 255}
-
-	// --- Disegna rettangolo ---
-	for x := 10; x < 118; x++ {
-		img.SetGray(x, 5, white)  // top
-		img.SetGray(x, 25, white) // bottom
-	}
-	for y := 5; y < 25; y++ {
-		img.SetGray(10, y, white)  // left
-		img.SetGray(118, y, white) // right
-	}
-
-	// --- Disegna testo ---
-	addLabel(img, 30, 18, "CIAO!")
-
-	// Scrivi su display
-	err = dev.Draw(img.Bounds(), img, image.Point{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	time.Sleep(10 * time.Second)
-}
-
-// Funzione per disegnare testo
-func addLabel(img *image.Gray, x, y int, label string) {
-	col := color.Gray{Y: 255}
-
-	point := fixed.Point26_6{
-		X: fixed.I(x),
-		Y: fixed.I(y),
-	}
-
-	d := &font.Drawer{
-		Dst:  img,
-		Src:  image.NewUniform(col),
-		Face: basicfont.Face7x13,
-		Dot:  point,
-	}
-
-	d.DrawString(label)
+	time.Sleep(10 * time.Second) // Delay di 2 secondi
+	dis.ClearDisplay()
+	dis.AddLabel(10, 30, "Ciao Paolo")
+	dis.DrawBitmap(display.IconBattery, 110, 8)
 }
