fd6ef94b0e9b94c6fe80d5ffe4765e8063d5f8e3
Paolo Manfe <panfe.maolo@gmail.com>
2026-04-01 09:16:08 +0200

    aggiunto funzione scatto foto

--- hw/camera.go
+++ hw/camera.go
@@ -3,7 +3,7 @@ package hw
 import "os/exec"
 
 func TakePhoto() []byte {
-	cmd := exec.Command("libcamera-still", "-o", "-", "--nopreview")
+	cmd := exec.Command("rpicam-still", "-o", "-", "--nopreview", "--timeout", "1000")
 	imgBytes, _ := cmd.Output()
 	return imgBytes
 }
