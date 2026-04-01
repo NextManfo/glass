d0c876b58d275389c15f9e8b16e957a61a3ff566
Paolo Manfe <panfe.maolo@gmail.com>
2026-04-01 09:34:38 +0200

    fix scatto foto

--- hw/camera.go
+++ hw/camera.go
@@ -3,7 +3,15 @@ package hw
 import "os/exec"
 
 func TakePhoto() []byte {
-	cmd := exec.Command("rpicam-still", "-o", "-", "--nopreview", "--timeout", "1000")
+	cmd := exec.Command(
+		"rpicam-still",
+		"-o", "-",
+		"--nopreview",
+		"--timeout", "1000",
+		"--width", "640",
+		"--height", "480",
+		"--quality", "50",
+	)
 	imgBytes, _ := cmd.Output()
 	return imgBytes
 }
